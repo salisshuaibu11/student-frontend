a679c474ff8046b526e5ce2770f945cafcf8aadc
salisu <salisshuaibu11@gmail.com>
2021-06-19 01:38:29 +0000

    Implemented Layouts

---
+++ src/routes/create-student.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Form, FormGroup, FormText, Input, Label, Button } from 'sveltestrap/src';
+</script>
+
+<div class="form-wrapper">
+	<Form>
+		<FormGroup>
+			<Label for="name">Name</Label>
+			<Input type="text" name="name" id="name" value="Enter your name" />
+		</FormGroup>
+	</Form>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import 'bootstrap/dist/css/bootstrap.css';
+</script>
+
+<p>Student List Student</p>
